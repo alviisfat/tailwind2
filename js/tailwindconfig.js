f3a4625ffe11c624b507f646d9d2e54b2e66ef54
isfathossainalvi <isfathossainalvi58@gmail.com>
2023-11-09 03:52:30 -0800

    first commit

---
+++ js/tailwindconfig.js
@@ -0,0 +1,49 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        // Hamara Code Starts
+        // Header Color Code Starts
+        colors: {
+         headercolor: "#1C1E53",
+         headingtext: "#BBBBCB",
+         hoverbtn: "#FCD980",
+         hovertext: "#1B1C2B",
+         bannercol: "#1C1E53",
+         whatwedotxt: "#282938",
+         paracol: "#676873",
+         linkcol: "#2405F2",
+         whatwedobg: "#F4F6FC",
+         bannerbg:"rgba(28, 30, 83, 0.656)",
+         buttoncolor1: "#FCD980",
+         featuresbg: "#F4F6FC",
+         clientbg: "#F9FBFD",
+        },
+        // Header Color Code Ends
+        // All Container Starts
+        width: {
+            headercontainer: "1170px",
+        },
+         // All Container Ends
+
+         // All Background Images code Starts
+         backgroundImage: {
+          
+         },
+         // All Background Images code Ends
+
+         // Font Family Starts
+         fontFamily: {
+          
+          poppins: ["Poppins", "sans-serif"],
+         },
+         // Font Family Ends
+         // box shadow starts
+         boxShadow: {
+         
+         },
+         // box shadow ends
+        // Hamara Code Ends
+         }
+      }
+    }
+  
